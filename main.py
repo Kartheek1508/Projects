814733b4db6a1dfb202a5f0ddb13ef3ac74e8c64
BNS Kartheek <91526876+Kartheek1508@users.noreply.github.com>
2021-10-24 17:34:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import discord
-from discord.ext import commands
-import os 
-import music
 
-client = commands.Bot(command_prefix = "?", intents = discord.Intents.all())
- 
-cogs = [music]
-
-for i in range(len(cogs)):
-  cogs[i].setup(client)
-
-client.run(os.getenv('TOKEN'))
